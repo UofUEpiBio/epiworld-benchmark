2920804fbc2ce91f6253a9cae7a0e5a296b4860e
George G. Vega Yon <g.vegayon@gmail.com>
2023-03-17 10:54:22 -0600

    Baseline setup of the simulation for SIR calibration

--- calibration/sir.R
+++ calibration/sir.R
@@ -8,14 +8,15 @@ ndays <- 100
 set.seed(1231)
 
 theta <- data.table(
-  preval = rbeta(N, 1, 19),     # Mean 1/(1 + 19) = 0.05
-  repnum = rgamma(N, 4, 4/1.5), # Mean 4/(4 / 1.5) = 1.5
-  ptran  = rbeta(N, 19, 1),     # Mean 19/(1 + 19) = 0.95
+  preval = rbeta(N, 1, 19),        # Mean 1/(1 + 19) = 0.05
+  repnum = rgamma(N, 4, 4/1.5),    # Mean 4/(4 / 1.5) = 1.5
+  ptran  = rbeta(N, 19, 1),        # Mean 19/(1 + 19) = 0.95
   prec   = rbeta(N, 10, 10*2 - 10) # Mean 10 / (10 * 2 - 10) = .5
 )
 
 ans <- vector("list", N)
 for (i in 1:N) {
+  
   m <- theta[i,
     ModelSIRCONN(
       "mycon",
@@ -26,7 +27,14 @@ for (i in 1:N) {
       n = n
       )
     ]
+  
   run(m, ndays = ndays)
   ans[[i]] <- get_hist_total(m)
+  
 }
+
+# Setting up the data for tensorflow. Need to figure out how we would configure
+# this to store an array of shape 3 x 100 (three rows, S I R) and create the 
+# convolution.
+
 
